eb75bc69df13a3de558861c1cf2c59201e427a05
davidgoulart-dev <davidgoulart.tae@gmail.com>
2024-08-26 13:34:22 -0300

    Colletions Set exercicio
    Signed-off-by: davidgoulart-dev <davidgoulart.tae@gmail.com>

---
+++ src/Main/List/OperacoesBasicas/Set/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,34 @@
+package Main.List.OperacoesBasicas.Set.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    //atributos
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+    public void removerConvidadoPorCodigoDeConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c: convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+}
---
+++ src/Main/List/OperacoesBasicas/Set/OperacoesBasicas/Convidado.java
@@ -0,0 +1,30 @@
+package Main.List.OperacoesBasicas.Set.OperacoesBasicas;
+
+public class Convidado {
+    //atributos
+    private String nome;
+
+    private int codigoConvite;
+
+    public Convidado(String nome, int codigoConvite) {
+        this.nome = nome;
+        this.codigoConvite = codigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigoConvite() {
+        return codigoConvite;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigoConvite=" + codigoConvite +
+                '}';
+    }
+}
